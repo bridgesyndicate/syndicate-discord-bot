a3e9472a81f7b01322bd72c3b35e3baed43edca9
Vice Versa <vice@bridgesyndicate.gg>
2021-06-20 20:43:34 -0700

    Added the first slash command, "/q"

---
+++ .toys.rb
@@ -0,0 +1,56 @@
+tool 'hello' do
+  def run
+    puts 'Hello, world!'
+  end
+end
+
+tool 'list-commands' do
+  flag :token, '--token TOKEN'
+  def run
+    require_relative 'discord_api'
+    client = DiscordApi.new(bot_token: token)
+    result = client.list_commands
+    puts JSON.pretty_generate(result)
+  end
+end
+
+tool 'create-command' do
+  flag :token, '--token TOKEN'
+
+  def run
+    require_relative 'discord_api'
+    client = DiscordApi.new(bot_token: token)
+    definition = {
+      name: "q",
+      description: "Hop into the scrim queue",
+      options: [
+        {
+          name: 'gamemode',
+          description: 'The size of teams',
+          type: 3,
+          required: true,
+          choices: [
+            {
+              name: '1v1',
+              value: '1v1'
+            },
+            {
+              name: '2v2',
+              value: '2v2'
+            },
+            {
+              name: '3v3',
+              value: '3v3'
+            },
+            {
+              name: '4v4',
+              value: '4v4'
+            }
+          ]
+        }
+      ]
+    }
+    result = client.create_command(definition)
+    puts JSON.pretty_generate(result)
+  end
+end
---
+++ discord_api.rb
@@ -0,0 +1,47 @@
+require "faraday"
+require "json"
+
+class DiscordApi
+    DISCORD_APPLICATION_ID = "856369764729618432"
+    DISCORD_GUILD_ID = "855996952348327946"
+    
+    def initialize(bot_token:)
+      @client_id = DISCORD_APPLICATION_ID
+      @guild_id = DISCORD_GUILD_ID
+      @bot_token = bot_token
+    end
+    
+    def list_commands
+      call_api("/applications/#{@client_id}/guilds/#{@guild_id}/commands")
+    end
+
+    def create_command(command_definition)
+      definition_json = JSON.dump(command_definition)
+      headers = {"Content-Type" => "application/json"}
+      call_api("/applications/#{@client_id}/guilds/#{@guild_id}/commands",
+              method: :post,
+              body: definition_json,
+              headers: headers)
+    end  
+    
+    private
+    
+    def call_api(path,
+               method: :get,
+               body: nil,
+               params: nil,
+               headers: nil)
+    faraday = Faraday.new(url: "https://discord.com") do |conn|
+      conn.authorization(:Bot, @bot_token)
+    end
+
+    response = faraday.run_request(method, "/api/v8#{path}", body, headers) do |req|
+      req.params = params if params
+    end
+    unless (200...300).include?(response.status)
+      raise "Discord API failure: #{response.status} #{response.body.inspect}"
+    end
+    return nil if response.body.nil? || response.body.empty?
+    JSON.parse(response.body)
+  end
+end
