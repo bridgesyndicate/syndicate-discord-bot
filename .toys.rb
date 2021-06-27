16e91926df1856771fc71b797ca0049e3cc136dc
Vice Versa <vice@bridgesyndicate.gg>
2021-06-27 12:43:33 -0700

    q command works

--- .toys.rb
+++ .toys.rb
@@ -1,3 +1,5 @@
+$token = 'ODU2MzY5NzY0NzI5NjE4NDMy.YNACfg.0EfzhPl44YdmsYvLl8RyTvVyHHs'
+
 tool 'hello' do
   def run
     puts 'Hello, world!'
@@ -5,32 +7,28 @@ tool 'hello' do
 end
 
 tool 'list-commands' do
-  flag :token, '--token TOKEN'
   def run
     require_relative 'discord_api'
-    client = DiscordApi.new(bot_token: token)
+    client = DiscordApi.new(bot_token: $token)
     result = client.list_commands
     puts JSON.pretty_generate(result)
   end
 end
 
 tool 'delete-command' do
-  flag :token, '--token TOKEN'
   flag :command_id, '--command-id ID'
   def run
     require_relative 'discord_api'
-    client = DiscordApi.new(bot_token: token)
+    client = DiscordApi.new(bot_token: $token)
     result = client.delete_command(command_id)
     puts JSON.pretty_generate(result)
   end
 end
 
-tool 'create-command' do
-  flag :token, '--token TOKEN'
-  
+tool 'create-command' do  
   def run
     require_relative 'discord_api'
-    client = DiscordApi.new(bot_token: token)
+    client = DiscordApi.new(bot_token: $token)
     definition = {
       name: "q",
       description: "Hop into the scrims queue",
