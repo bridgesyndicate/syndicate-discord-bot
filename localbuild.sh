c05321d4d1e49f93c2f57f7b1c8449eabb8ba967
Kenneth Berland <ken@hero.net>
2021-09-23 21:25:26 -0700

    fix local run

--- localbuild.sh
+++ localbuild.sh
@@ -2,4 +2,8 @@
 set -e
 
 ./localbuild.rb | grep -v 'aws ecr' | bash
-docker run -it 595508394202.dkr.ecr.us-west-2.amazonaws.com/syn-bot:latest
+docker run -it \
+       -e AWS_REGION=us-west-2 \
+       -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+       -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+       595508394202.dkr.ecr.us-west-2.amazonaws.com/syn-bot:latest
