d6c7ab5f12c0afb9381ee918d4c37f9d34c5f0ab
Kenneth Berland <ken@hero.net>
2021-09-29 21:11:52 -0700

    add ranked starter here

--- Gemfile
+++ Gemfile
@@ -13,3 +13,11 @@ gem 'aws-sigv4'
 gem 'aws-sdk-secretsmanager'
 gem 'aws-sdk-sqs'
 gem 'pry-byebug'
+
+group :test do
+  gem 'simplecov', require: false, group: :test
+  gem 'rspec'
+  gem 'rspec_junit_formatter'
+  gem 'timecop'
+  gem 'webmock'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require 'simplecov'
+require 'webmock/rspec'
+
+SimpleCov.start
+
+ENV['SYNDICATE_ENV'] = 'test'
+ENV['AWS_REGION'] = 'us-west-2'
+
+Bundler.require(:default, 'test')
+
+root = File.expand_path("..", File.dirname(__FILE__))
+$LOAD_PATH.unshift(root) unless $LOAD_PATH.include?(root)
+
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
+
+# WebMock.allow_net_connect!
