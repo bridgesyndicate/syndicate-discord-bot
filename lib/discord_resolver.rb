d4d9003bcc999cf168c366518945980d4adadc8c
Kenneth Berland <ken@hero.net>
2022-08-18 22:39:50 -0700

    add debugging to name resolution

--- lib/discord_resolver.rb
+++ lib/discord_resolver.rb
@@ -7,6 +7,8 @@ class DiscordResolver
   end
 
   def resolve_name_from_discord_id(discord_id)
-    bot.server(server_id).member(discord_id).username
+    username = bot.server(server_id).member(discord_id).username
+    syn_logger "DiscordResolver: resolved #{discord_id} as #{username}"
+    return username
   end
 end
